21c969817119c6468ba770341b371ee830ec2dea
Alex Didenko <alex.didenko753@gmail.com>
2022-04-23 17:40:24 +0400

    3 секунды, распарелил

--- signer.go
+++ signer.go
@@ -5,8 +5,14 @@ import (
 	"log"
 	"sort"
 	"strconv"
+	"sync"
 	"time"
 )
+
+// inputData := []int{11, 22, 33, 44}
+// var inputData = []int{0, 1, 2}
+// var inputData = []int{0}
+var inputData = []int{0, 1, 1, 2, 3, 5, 8}
 
 func main() {
 	j1 := SingleHash
@@ -15,7 +21,6 @@ func main() {
 	Jobs := []job{job(j1), job(j2), job(j3)}
 	ExecutePipeline(Jobs...)
 	fmt.Println("finish")
-	fmt.Scanln()
 }
 func ExecutePipeline(jobs ...job) {
 	start := time.Now()
@@ -26,9 +31,6 @@ func ExecutePipeline(jobs ...job) {
 	// in3 := make(chan interface{}, 10)
 	out3 := make(chan interface{}, 10)
 
-	// inputData := []int{11, 22, 33, 44}
-	inputData := []int{0, 1, 1, 2, 3, 5, 8}
-
 	for _, fibNum := range inputData {
 		in1 <- fibNum
 	}
@@ -38,12 +40,17 @@ func ExecutePipeline(jobs ...job) {
 		switch i {
 		case 0:
 			job(in1, out1)
+			elapsed := time.Since(start)
+			log.Printf("Execution time %s", elapsed)
 		case 1:
 			job(out1, out2)
+			elapsed := time.Since(start)
+			log.Printf("Execution time %s", elapsed)
 		case 2:
 			job(out2, out3)
 		}
 	}
+	close(out3)
 
 	fmt.Println("ExecutePipeline finish")
 	elapsed := time.Since(start)
@@ -52,45 +59,71 @@ func ExecutePipeline(jobs ...job) {
 
 func SingleHash(in, out chan interface{}) {
 	fmt.Println("job0 start")
-
-	var results []string
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
 	for rawIn := range in {
+		wg.Add(1)
 		data := fmt.Sprintf("%v", rawIn)
-		fmt.Println("job0 rawIn", data)
-
-		result := DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
-		results = append(results, result)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			fmt.Println("job0 rawIn", data)
+			// todo parallel hash operations
+			mu.Lock()
+			dataMd5 := DataSignerMd5(data)
+			mu.Unlock()
+			result := DataSignerCrc32(data) + "~" + DataSignerCrc32(dataMd5)
+			out <- result
+		}(wg)
 	}
 
-	for _, element := range results {
-		out <- element
-	}
-
+	wg.Wait()
 	close(out)
 }
 
 func MultiHash(in, out chan interface{}) {
 	fmt.Println("job1  start")
-
-	var results []string
-
-	th := []int{0, 1, 2, 3, 4, 5}
-	for rawIn := range in {
-		data := fmt.Sprintf("%v", rawIn)
+	var calculateTh = func(data string, th []int, resultStrings chan interface{}, wgExternal *sync.WaitGroup) {
+		defer wgExternal.Done()
+		wgInternal := &sync.WaitGroup{}
+		mu := &sync.Mutex{}
+		subResultsMap := map[int]string{}
 		resultString := ""
 		for _, thItem := range th {
-			thItem := strconv.Itoa(thItem)
-			subResult := DataSignerCrc32(thItem + data)
-			fmt.Println(data + " MultiHash: crc32(th+step1)) " + thItem + " " + subResult)
-			resultString += subResult
+
+			wgInternal.Add(1)
+			i := strconv.Itoa(thItem)
+			thItem := thItem
+			go func(wg *sync.WaitGroup) {
+				defer wg.Done()
+				subResult := DataSignerCrc32(i + data)
+				fmt.Println(data + " MultiHash: crc32(th+step1)) " + i + " " + subResult)
+				mu.Lock()
+				subResultsMap[thItem] = subResult
+				mu.Unlock()
+			}(wgInternal)
 		}
-
-		results = append(results, resultString)
+		wgInternal.Wait()
+		// sort map
+		keys := make([]int, 0, len(subResultsMap))
+		for k := range subResultsMap {
+			keys = append(keys, k)
+		}
+		sort.Ints(keys)
+		for _, k := range keys {
+			fmt.Println("subResultsMap[k]", k, subResultsMap[k])
+			resultString += subResultsMap[k]
+		}
+		resultStrings <- resultString
 	}
 
-	for _, element := range results {
-		out <- element
+	th := []int{0, 1, 2, 3, 4, 5}
+	wg := &sync.WaitGroup{}
+	for rawIn := range in {
+		data := fmt.Sprintf("%v", rawIn)
+		wg.Add(1)
+		go calculateTh(data, th, out, wg)
 	}
+	wg.Wait()
 	close(out)
 }
 
